a9eca5152f4507d21ec8af6ddab3bf0b276b2721
Jafar Ansari <jafaransari@Jafars-MacBook-Pro.local>
2020-10-20 10:39:41 +0530

    initial commit

---
+++ ReadMe(Todo List).txt
@@ -0,0 +1,23 @@
+Hello Mohammed, Here are the instructions for the subdomain and DB 
+
+Step Zero: 
+    - create github repo for this site/project
+
+Step One:  
+    - import Emerson Database into wincmedi_CuratedSupplementsProducts
+        - API Docs located here: https://www.emersonproductsync.wincmedia.com/emerson-api-docs/
+        - Product Data endpoint is in a .txt file called "endpoint.txt"
+        - Schema outline located here: https://www.emersonproductsync.wincmedia.com/schema-docs/
+    
+Step Two:
+    - provide access to database through DatabaseForm.php (or another page)
+    - allow a user to select product by SKU
+    - allow a user to select group of products by brand
+    - display data in a useful manner (we can work on this together)
+
+
+
+MYSQL/MariaDB:
+    - User: wincmedi_Mohammed
+    - Database: wincmedi_CuratedSupplementsProducts
+    - DB user pass: * same as FTP login *
---
+++ index.php
@@ -0,0 +1,43 @@
+<!-- welcome mohammed! -->
+<?php
+
+// php stuff :) 
+
+?>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Curated Supplements | Emerson Product Sync</title>
+    <meta name="description" content="Curated Supplements Product Database (sync'ed with emerson ecologics)">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+
+<body style="background-color: #0f0f0f;">
+    <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+    <!-- Begin Nav Bar / Menu Bar -->
+    <?php require_once('navbar.html'); ?>
+    <!-- End Nav Bar / Menu Bar  -->
+
+    <div class="container-fluid">
+        <div class="container" id="mainContainer">
+            <h1 class="text-light"> Curated Supplements | Emerson Product Sync <h1>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
