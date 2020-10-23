3fa3545cac605a2c9cb8fc8483c57086a9ba4b06
Jafar Ansari <jafaransari@Jafars-MacBook-Pro.local>
2020-10-23 17:30:09 +0530

    update funnction

--- function.php
+++ function.php
@@ -9,27 +9,6 @@ include_once('db.php');
  *    insertArr("tablename", $data);
  */
 
- $insData = array(
-    'productId' => 1,
-    'productName' => '1 Potassium Trace Minerals',
-    'brandID' => 921,
-    'description'=> 'DIetary Supplement Potassium is essential for healthy cell function, nerve transmission and pH balance.',
-    'labelText'=> 'test',
-    'suggestedUse'=> 'Suggested Use:As a mineral supplement, add 13 drops in water or juice daily or more as directed by a licensed healthcare practitioner.',
-    'warnings'=> 'Keep out of the reach of children. Store in a cool, dry place.',
-    'deliveryMeasure'=> 'Ounces',
-    'sku'=> 'POT10',
-    'itemName'=> '1 Potassium Trace Minerals 4 oz, 1 Potassium Trace Minerals 2 oz,',
-    'itemStatus'=> 1,
-    'itemUPC'=> 743474440117,
-    'itemProp65Restriction'=> 1,
-    'itemProp65Alternative'=> '2',
-    'itemBrandSku'=> 'LM401',
-    'itemImageURL'=> 'https://static.emersonecologics.com/c8e3d146-7ba3-47f3-90b1-ed602cdf16c5-358-358.png',
-    'itemImageHeight'=> 358,
-    'itemImageWidth'=> 358,
-    'itemImagePresent'=> '1'
-);
 insert('Products', $insData);
 
 function insert($tableName, $insData){
